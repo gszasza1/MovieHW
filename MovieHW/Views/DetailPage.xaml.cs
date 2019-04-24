006ed8120b6353ae2601d48902602b807cbc0c2c
gszasza1 <gszasza1@gmail.com>
2019-04-25 00:25:59 +0200

    initial commit

---
+++ MovieHW/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MovieHW.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace MovieHW.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
